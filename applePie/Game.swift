0b13a8f18079bfbaf58529ee30dc85ea34bb40c7
Sarah Strickley <sarahstrickley@cwi.edu>
2023-07-31 13:41:34 -0600

    Uit is complete. UI completes properly

---
+++ applePie/Game.swift
@@ -0,0 +1,19 @@
+//
+//  Game.swift
+//  applePie
+//
+//  Created by admin on 7/31/23.
+//
+//Mention this: We know this is a data file because it says import Foundation, and not import UIKit
+//COCOTouch will be UIKit
+
+
+//If we are going to control the things in view controlller, they need to be added to the struct
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    
+}
--- applePie/ViewController.swift
+++ applePie/ViewController.swift
@@ -8,12 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - IB Outlets
+    
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    //change style in attributes inspector to Default, not Plain
+    @IBOutlet var letterButtons: [UIButton]!
+    
+    //MARK: - Instance Properties
+    var listOfWords = ["buccaneer", "swift", "glorious", "incandescent","bug", "program", "cheese"]
+    
+    let incorrectMovesAllowed = 7
+    
+    var totalWins = 0
+    var totalLosses = 0
+    
+    var currentGame: Game!
+    
+    //MARK: - VDL
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //In the course, this is build before the function newRound() is declared, we can build that out under instance methods to avoid the error.
+        newRound()
     }
+    
+    
+    //MARK: - IB Actions
+    
+    @IBAction func letterButtonTapped(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
+    
+    
+    //MARK: - Instance Methods
 
-
+    //starts a new found of the game
+    func newRound() {
+        let newWord = listOfWords.removeFirst()
+        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed)
+        updateUI()
+    }
+   
+    func updateUI(){
+        scoreLabel.text = "Wins: \(totalWins) | Losses: \(totalLosses)"
+        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
+    }
 }
+
+
+//whatever I name my file is what I will name my struct
+
 
